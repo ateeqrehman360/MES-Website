803ebe5ab5df888c4136020b017d807e79a73a39
ateeqrehman360 <ateeqrehman360@outlook.com>
2026-09-11 03:59:00 +0100

    polish journey safari motion and mobile path

--- src/components/sections/journey-section.tsx
+++ src/components/sections/journey-section.tsx
@@ -28,6 +28,7 @@ export function JourneySection() {
                 data-importance={milestone.importance}
                 data-placement={milestone.placement}
                 data-route={milestone.route}
+                data-mobile-route={milestone.mobileRoute}
                 data-has-media={milestone.image ? "true" : "false"}
               >
                 <span
